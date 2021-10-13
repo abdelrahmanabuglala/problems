d1e3c263ce33bfd09a7398c0d153f1ad1e7f73e0
abdelrahmanabuglala <77413214+abdelrahmanabuglala@users.noreply.github.com>
2021-10-13 16:30:52 +0200

    template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void fl() {
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+//   freopen("ot.txt", "w", stdout);
+#else
+//  freopen("jumping.in", "r", stdin);  // HERE
+#endif
+}
+
+int main()
+{
+    fl();
+    return 0;
+}
