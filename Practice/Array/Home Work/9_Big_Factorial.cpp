48f89d5f1d73ff942fbd8e8cc2cbe1fb2b8204ee
abdelrahmanabuglala <77413214+abdelrahmanabuglala@users.noreply.github.com>
2021-11-22 01:08:17 +0200

    Array_HW

--- Practice/Array/Home Work/9_Big_Factorial.cpp
+++ Practice/Array/Home Work/9_Big_Factorial.cpp
@@ -16,7 +16,7 @@ int main()
   fl();
   int n;
   cin >> n;
-  int arr[1001];
+  int arr[10001] = {0};
   arr[0] = 1;
   if (n == 0 || n == 1)
   {
@@ -24,25 +24,40 @@ int main()
   }
   else
   {
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i <= n; i++)
     {
-      for (int j = 0; j < 1000; j++)
-      {
+      for (int j = 0; j < 10000; j++)
         arr[j] *= i;
+
+      for (int j = 0; j < 10000; j++)
         if (arr[j] > 9)
         {
           int carry = arr[j] / 10;
           arr[j] = arr[j] % 10;
           arr[j + 1] += carry;
         }
-      }
     }
     string res = "";
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < 10000; i++)
     {
       res = to_string(arr[i]) + res;
     }
-    cout << res;
+    int x;
+    for (int i = 0; i < 10000; i++)
+    {
+      if (res[i] != '0')
+      {
+        x = i;
+        break;
+      }
+      
+    }
+    for (int i = x; i < res.size(); i++)
+    {
+      cout<<res[i];
+    }
+    
+    
   }
 
   return 0;
