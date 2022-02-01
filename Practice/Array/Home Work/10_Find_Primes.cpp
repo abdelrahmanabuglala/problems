7dc3c1a91e0476bbed50a9b52e519daf5484d4fd
abdelrahmanabuglala <77413214+abdelrahmanabuglala@users.noreply.github.com>
2022-02-01 22:06:42 +0200

    Problems_A

---
+++ Practice/Array/Home Work/10_Find_Primes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fl()
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+//   freopen("ot.txt", "w", stdout);
+#else
+//  freopen("jumping.in", "r", stdin);  // HERE
+#endif
+}
+
+int main()
+{
+    fl();
+    int n;
+    cin >> n;
+    int arr[1000] = {1};
+    cout << arr[100];
+    return 0;
+}
